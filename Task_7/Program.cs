5285dd3b97c19b1aa253f5adac6b95fee9a3e13a
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-03-03 16:50:50 +0300

    Task_7

---
+++ Task_7/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int N = int.Parse(Console.ReadLine());
+int B = N % 10;
+
+
+Console.WriteLine(B);
+
+Console.Write($"{B} ");
+
