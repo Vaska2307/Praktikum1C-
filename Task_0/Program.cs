9772c7d116e7b3756693f82aa9bca16f94334e68
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-03-03 13:58:59 +0300

    Task 0

---
+++ Task_0/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear();
+Console.Write("Ввудите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа = {sqr}");
+
+int sqr1 = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Квадрат числа = {sqr1}");
