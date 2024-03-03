6de4d1692240aeb1c0d78664006735a93d4a9a8e
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-03-03 15:01:44 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.Write("Введите первое число: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+int result = Convert.ToInt32(Math.Pow(number2, 2));
+
+if (number == result)
+{
+    Console.WriteLine($"Число {number} является квадратом числа {number2}");
+}
+
+else
+{
+    Console.WriteLine($"Число {number} не является квадратом числа {number2}"); 
+}
