0d20e93a9b8a3566cbdb80285645d02053248754
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-03-12 17:55:56 +0300

    Task_14

---
+++ Task_14/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+
+if ((a % 7 == 0)&&(a % 23 == 0))
+{
+    WriteLine($"{a} -> да ");
+}
+else
+{
+    WriteLine($"{a} -> нет ");
+}
