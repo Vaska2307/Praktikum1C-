2bbc713af91dfd80737f9e9f1d9dd723bb260ee0
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-03-12 17:34:34 +0300

    Task_11

---
+++ Task_11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите превое число: ");
+int number1 = int.Parse(Console.ReadLine());
+Write("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine());
+
+if (number1 % number2 == 0)
+{
+    WriteLine($"{number1} кратно ");
+}
+else
+{
+    WriteLine($"{number1} не кратно, остаток {number1 % number2}");
+}
+
