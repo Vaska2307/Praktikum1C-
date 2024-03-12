2633ff8611a48b53012f341c24820a60be13076b
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-03-12 21:38:57 +0300

    Task_16

---
+++ Task_16/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите первое число: ");
+int a = int.Parse(ReadLine());
+Write("Введите второе число: ");
+int b = int.Parse(ReadLine());
+
+if (b * b == a)
+{
+    WriteLine($"a = {a}, b = {b} -> a квадрат b");
+}
+else
+{
+    if(Math.Pow(a,2)==b)
+    {
+        WriteLine($"a = {a}, b = {b} -> b квадрат a");
+    }
+    else
+    {
+       WriteLine($"a = {a}, b = {b} -> не является"); 
+    }
+}
