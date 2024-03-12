9f3f9131bebb7264a588b9766c9240c0bd3e7536
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-03-12 22:42:44 +0300

    Task_13

---
+++ Task_13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите трехзначное число: ");
+int a1 = int.Parse(ReadLine());
+int a2 = new Random().Next(1, 10000);
+if(a1 < 100)
+{
+    WriteLine($"{a1} -> трерьей цифры нет");
+}
+if (a1 > 100)
+{
+    WriteLine($"{a1} -> {a1 % 10}");
+}
+if(a2 < 100)
+{
+    WriteLine($"{a2} -> трерьей цифры нет");
+}
+if (a2 > 100)
+{
+    WriteLine($"{a2} -> {a2 % 10}");
+}
+
