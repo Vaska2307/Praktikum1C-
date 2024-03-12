478cea9085769828a4b0e809c21631a245dfd65e
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-03-12 22:18:38 +0300

    Task_8

---
+++ Task_8/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите трехзначное число: ");
+int number = int.Parse(ReadLine());
+
+WriteLine($"{number}->{(number % 100) / 10} ");
+
+
