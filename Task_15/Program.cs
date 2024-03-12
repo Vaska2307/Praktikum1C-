6c4d301696cad1d1e8485b6fb66150fb1e3e7021
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-03-12 23:44:57 +0300

    Task_15

---
+++ Task_15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using static System.Console;
+Clear();
+Write("Введите число: ");
+int a = int.Parse(ReadLine());
+
+if(a < 6)
+{
+    WriteLine($"{a} -> нет");
+}
+if (a > 7)
+{
+    WriteLine($"{a} -> нет");
+}
+else
+{
+    WriteLine($"{a} -> да");
+}
