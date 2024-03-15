9622042117e461b7df1a8e0fe8ce201226591273
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-03-15 13:12:48 +0300

    Task_17

---
+++ Task_17/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите координату Х: ");
+int X = int.Parse(ReadLine());
+Write("Введите координату У: ");
+int Y = int.Parse(ReadLine());
+
+if (X > 0&&Y > 0)
+{
+    WriteLine($"Четверть номер 1 ");
+}
+if (X < 0&&Y > 0)
+{
+    WriteLine($"Четверть номер 2 ");
+}
+if (X < 0&&Y < 0)
+{
+    WriteLine($"Четверть номер 3 ");
+}
+if (X > 0&&Y < 0)
+{
+    WriteLine($"Четверть номер 4 ");
+}
