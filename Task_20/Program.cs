662367dd26868746bde127bd83e2ad6d21159dea
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-03-15 15:14:18 +0300

    Task_20

---
+++ Task_20/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите координату X1: ");
+int x1 = int.Parse(ReadLine());
+Write("Введите координату Y1: ");
+int y1 = int.Parse(ReadLine());
+Write("Введите координату X2: ");
+int x2 = int.Parse(ReadLine());
+Write("Введите координату Y1: ");
+int y2 = int.Parse(ReadLine());
+
+
+
+double distance = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+WriteLine($"Длинна отрезка равна = {distance:f2}");
+
+
+
+
+
+
+
+
