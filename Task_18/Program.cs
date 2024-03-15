fb921bf87e12b22c831546d14603e3d388243790
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-03-15 13:56:02 +0300

    Task_18

---
+++ Task_18/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write("Введите номер четверти: ");
+int number = int.Parse(ReadLine());
+
+switch(number)
+{
+    case 1:
+    {
+        WriteLine("X, Y");
+        break;
+    }
+    case 2:
+    {
+        WriteLine("-X, Y");
+        break;
+    }
+    case 3:
+    {
+        WriteLine("-X, -Y");
+        break;
+    }
+    case 4:
+    {
+        WriteLine("X, -Y");
+        break;
+    }
+    default:
+    {
+        WriteLine("Неверная четверть");
+        break;
+    }
+
+}
