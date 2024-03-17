f912e5d1596a3beef2be987948851432285525d7
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-03-17 14:46:10 +0300

    Task_23

---
+++ Task_23/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Введите число: ");
+int N = int.Parse(ReadLine());
+
+for(int i = 1; i <= N; i++)
+{
+    WriteLine($"{Math.Pow(i ,3)} ");
+
+}
