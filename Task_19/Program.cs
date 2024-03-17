f3a47e39998d558a4cace3242b769af28a445388
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-03-17 13:35:07 +0300

    Task_19

---
+++ Task_19/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+int number = int.Parse(ReadLine());
+if(number < 10000 || number > 99999)
+{
+    WriteLine("Число не пятизначное");
+    return;
+}
+int a1 = number / 10000;
+number -= a1 * 10000;
+int a2= number / 1000;
+number -= a2 * 1000;
+int a3 = number / 100;
+number -= a3 * 100;
+int a4 = number / 10;
+int a5 = number % 10;
+
+if ((a1 == a5) && (a2 == a4))
+{
+    WriteLine("Число полиндром");
+}
+else
+{
+    WriteLine("Число не полиндром");
+}
