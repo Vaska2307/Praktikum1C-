cd0e1231e36bcad924375c801a927d00003dfabd
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-04-12 20:02:48 +0300

    Task_24

---
+++ Task_24/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите число N: ");
+int N = int.Parse(ReadLine());
+int summa = GetSumNumbers(N);
+WriteLine($"Сумма = {summa}");
+
+
+
+int GetSumNumbers(int number)
+{
+    int sum = 0;
+    for(int i = 1; i <= number; i ++)
+    {
+        sum += i;
+    }
+    return sum;
+}
