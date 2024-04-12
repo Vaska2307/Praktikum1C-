808f70a9d7ddd7cc66eaf5d736e44980fd0c678f
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-04-12 23:08:39 +0300

    Task_26

---
+++ Task_26/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите число N: ");
+WriteLine($"Произведение от 1 до N = {GetCompositionNumber(int.Parse(ReadLine()))}");
+
+
+int GetCompositionNumber(int number)
+{
+    int composit = 1;
+    for(int i = 2; i <= number; i++)
+    {
+        composit *= i;
+    }
+    return composit;
+}
