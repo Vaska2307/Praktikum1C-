340937ecdec3542623ec753776fb3f3c39256286
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-04-12 20:45:08 +0300

    Task_25

---
+++ Task_25/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Введите число: ");
+WriteLine($"Колличество цифр в числе = {GetCountNumbers(int.Parse(ReadLine()))}");
+
+
+
+int GetCountNumbers(int number)
+{
+    int count = 0;
+    while(number > 0)
+    {
+        count++;
+        number /=10;
+    }
+    return count;
+}
