1fbf5dcfe525181b01fdac1d9c8c166d8eae1daa
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-04-18 18:43:18 +0300

    Task-32

---
+++ Task_32/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+int[] array = GetReplaceNum(8, -10, 10);
+WriteLine(String.Join(" ", array));
+InversArray(array);
+WriteLine(String.Join(" ", array));
+
+int[] GetReplaceNum(int size, int min, int max)
+{
+    int[] result = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max +1);
+    }
+    return result;
+}
+void InversArray(int[] inArray)
+{
+    for(int i =1; i < inArray.Length; i++)
+    {
+        inArray[i]*=-1;
+    }
+}
