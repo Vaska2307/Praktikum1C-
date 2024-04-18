3e16cd2c642c022429ff2cc4b0308b1fa89a029b
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-04-18 17:23:38 +0300

    Task_31

---
+++ Task_31/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+int[] array = GetMasiiweSum(12, -9, 9);
+WriteLine(String.Join(",", array));
+int positiveSum = 0;
+int negativeSum = 0;
+foreach(int item in array)// foreach - это перебор элементов
+{
+    Write($"{item} ");
+    if (item < 0)
+    {
+       negativeSum = negativeSum + item;
+    }
+    else
+    {
+        positiveSum = positiveSum + item;
+    }
+}
+WriteLine();
+WriteLine($"Сумма положительных элементов = {positiveSum}, Сумма отрицательных элементов = {negativeSum}");
+
+
+int[] GetMasiiweSum(int size, int min, int max)
+{
+    int[] result = new int[size];
+    for(int i = 0; i < size; i++)
+    {
+        result[i] = new Random().Next(min, max + 1);
+    }
+    return result;
+}
+
