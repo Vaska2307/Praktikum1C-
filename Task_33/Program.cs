612552425171cce555e10b387d2e61e8c685116b
Vasiliy Gusakov <vasili.gusako@yandex.ru>
2024-04-18 22:02:59 +0300

    Task_33

---
+++ Task_33/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+WriteLine("Введите через пробел массив: ");
+int[] array = GetArrayFromString(ReadLine());
+
+Write("Введите элемент: ");
+int element = int.Parse(ReadLine());
+
+if(FinedElement(array, element))
+{
+    WriteLine("Да");
+}
+else
+{
+    WriteLine("Нет");
+}
+
+int[] GetArrayFromString(string stringArray)
+{
+    string[] numS = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] result = new int [numS.Length];
+    for(int i = 0; i < result.Length; i++)
+    {
+        result[i] = int.Parse(numS[i]);  
+    }
+    return result;
+}
+
+bool FinedElement(int[] inArray, int el)
+{
+    foreach(int item in inArray)
+    {
+        if(item == el) return true;
+    }
+    return false;
+}
